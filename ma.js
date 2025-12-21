039e0638fc7810435f494897d675076686a9fe6b
shuhou98374 <154719810+shuhou98374@users.noreply.github.com>
2025-12-21 14:56:01 +0900

    ファイル追加
    コードファイルを追加

---
+++ ma.js
@@ -0,0 +1,13 @@
+const loading = document.querySelector('#loading');
+
+window.addEventListener('load', () => {
+  loading.classList.add('loaded');
+});
+
+const items = document.querySelector('.midashi');
+
+const keyframes={
+    opacity:[0,1]
+};
+
+
